50b275f0f3ccbbc6e5deeff11e743b393024df53
Youssef Hossam <155332489+YoussefHosssam@users.noreply.github.com>
2025-03-12 23:05:16 +0200

    Update problem4.cpp add Bubble , Shell sort

--- problem4.cpp
+++ problem4.cpp
@@ -1,11 +1,12 @@
-
-#ifndef SORTING_SYSTEM_H
-#define SORTING_SYSTEM_H
-
 #include <iostream>
 #include <limits>
-
 using namespace std;
+
+void swap (int &x , int &y){
+    x = x ^ y ;
+    y = x ^ y ;
+    x = x ^ y ;
+}
 
 template <typename T>
 class SortingSystem {
@@ -35,12 +36,10 @@ public:
 
     // Utility Functions
     void displayData();
+    void displaySortedData();
     void measureSortTime(void (SortingSystem::*sortFunc)());
     void showMenu();
 };
-
-#endif 
-
 template <typename T>
 SortingSystem<T>::SortingSystem(int n) {
     size = n;
@@ -65,12 +64,33 @@ void SortingSystem<T>::selectionSort() {
 
 template <typename T>
 void SortingSystem<T>::bubbleSort() {
-    // Implementation here
+    for (int i = 0 ; i < this->size - 1 ; i++){
+        for (int j = this->size-1 ; j > i ; j--){
+            if (this->data[j] < this->data[j-1]){
+                cout << "Element [ "<<data[j-1] << " ] is bigger than [ " << data[j] << " ] so swap between them ... \n" ;
+                swap (this->data[j-1] , this->data[j]);                
+                displaySortedData();
+            }
+        }
+    }
+    cout << "\nThis is 3-cases algorithm which takes \nBest case : O(n)\nAverage case : O(n^2)\nWorst case : O(n^2)" ;
+    
 }
 
 template <typename T>
-void SortingSystem<T>::shellSort() {
-    // Implementation here
+void SortingSystem<T>::shellSort() { 
+    for (int gap = size / 2 ; gap > 0 ; gap/=2){
+        for (int j = gap ; j < size ; j++){
+            for (int i = j ; i >=gap ; i-=gap){
+                if (data[i-gap] > data[i]){
+                    swap(data[i-gap] , data[i]);
+                    cout << "Gap is equal "<<gap<< " and element [ "<<data[i] << " ] is bigger than [ " << data[i-gap] << " ] so swap between them ... \n" ;
+                    displaySortedData() ;
+                }
+            }
+        }
+    }
+    cout << "\nThis is 3-cases algorithm which takes \nBest case : O(n)\nAverage case : O(n^1.5)\nWorst case : O(n^2)" ;
 }
 
 template <typename T>
@@ -121,6 +141,19 @@ void SortingSystem<T>::displayData() {
     cout << "]"<<endl;
     
 }
+template <typename T>
+void SortingSystem<T>::displaySortedData() {
+    cout << "Sorted Data: [";
+    for (int i = 0; i < size; i++) {
+        cout << data[i];
+        if (i < size - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]"<<endl;
+    
+}
+
 
 template <typename T>
 void SortingSystem<T>::measureSortTime(void (SortingSystem::*sortFunc)()) {
